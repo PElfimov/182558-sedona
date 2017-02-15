7f88a804f878c1a26c6274229985cdc8d8e2bd14
gonzik82 <pelfimov@ya.ru>
2017-02-15 21:50:06 +0700

    Не включил в прошлый комит скрипт

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var foundhotel = document.querySelector(".page-content-index-form-btn-open-closed");
 var popup = document.querySelector(".page-content-index-appointment-form");
-foundhotel.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.toggle("page-content-index-appointment-form-close");
-  console.log("Клик по ссылке вход");
-});
+if (popup) {
+  var foundhotel = document.querySelector(".page-content-index-form-btn-open-closed");
+  popup.classList.add("page-content-index-appointment-form-close");
+  foundhotel.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.toggle("page-content-index-appointment-form-close");
+    console.log("Клик по ссылке вход");
+  });
+}
